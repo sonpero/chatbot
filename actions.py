f08b030ed71d24c197d7d09723e446ff89f0dea2
sonpero <sonpero@hotmail.com>
2020-04-03 15:04:59 +0200

    Fonctionnalité d'envoi de mail opérationnel : récupére le message, l'adresse mail et le nom

--- actions.py
+++ actions.py
@@ -5,27 +5,6 @@
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
 from rasa_sdk.forms import FormAction
 from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
@@ -54,14 +33,6 @@ class InfoMail (FormAction):
             "person_name",
         ]
 
-    #def submit(
-    #        self,
-    #        dispatcher: CollectingDispatcher,
-    #        tracker: Tracker,
-    #        domain: Dict[Text, Any],
-    #    ) -> List[Dict]:
-    #    dispatcher.utter_message("Merci, votre message a bien été envoyé")
-    #    return []
 
     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
 
